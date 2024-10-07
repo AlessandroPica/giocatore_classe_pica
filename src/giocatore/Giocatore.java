e89e06d544112ac3e7b0af25caaf003cdac5e391
AlessandroPica <pica.alessandro.studente@itispaleocapa.it>
2024-10-07 12:57:18 +0200

    costruttore pieno

--- src/giocatore/Giocatore.java
+++ src/giocatore/Giocatore.java
@@ -2,14 +2,17 @@ package giocatore;
 
 public class Giocatore {
     private String nome;
-    private String capitano;
+    private boolean capitano;
     private int goal;
 
     public Giocatore (){
         nome = "";
-        capitano="";
+        capitano=false;
         goal = 0;
     }
-
-
+    public Giocatore (String n, boolean cap,int g){
+        nome = n;
+        capitano = cap;
+        goal = g;
+    }
 }
